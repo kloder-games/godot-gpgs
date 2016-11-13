c8231bac42390024957b249459533b697eba6b6f
Javier López Úbeda <jlopezcur@gmail.com>
2016-11-13 14:39:38 +0100

    Upgrade to 9.8.0 version of Google Play Game Services

--- gpgs/config.py
+++ gpgs/config.py
@@ -3,9 +3,9 @@ def can_build(plat):
 
 def configure(env):
     if env['platform'] == 'android':
-        env.android_add_dependency("compile 'com.google.android.gms:play-services-plus:9.2.1'")
-        env.android_add_dependency("compile 'com.google.android.gms:play-services-drive:9.2.1'")
-        env.android_add_dependency("compile 'com.google.android.gms:play-services-games:9.2.1'")
+        env.android_add_dependency("compile 'com.google.android.gms:play-services-plus:9.8.0'")
+        env.android_add_dependency("compile 'com.google.android.gms:play-services-drive:9.8.0'")
+        env.android_add_dependency("compile 'com.google.android.gms:play-services-games:9.8.0'")
         env.android_add_java_dir("android")
         env.android_add_to_manifest("android/AndroidManifestChunk.xml")
         env.disable_module()
